9e2fb43c4ca5f94053d063444f609c704c04acc1
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-03-24 14:19:21 +0200

    Ota uusi logikonfiguraatio käyttöön

--- src/clj/hakukohderyhmapalvelu/core.clj
+++ src/clj/hakukohderyhmapalvelu/core.clj
@@ -1,16 +1,9 @@
 (ns hakukohderyhmapalvelu.core
   (:require [com.stuartsierra.component :as component]
             [hakukohderyhmapalvelu.system :as system]
-            [taoensso.timbre :as timbre]
-            [taoensso.timbre.appenders.core :as appenders])
+            [hakukohderyhmapalvelu.timbre-config :as timbre-config])
   (:gen-class))
 
-(defn- configure-logging []
-  (timbre/merge-config!
-    {:level     :info
-     :appenders {:println (appenders/println-appender
-                            {:stream :std-out})}}))
-
 (defn -main [& _args]
-  (configure-logging)
+  (timbre-config/configure-logging!)
   (component/start-system system/hakukohderyhmapalvelu-system))
