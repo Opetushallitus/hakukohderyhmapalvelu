282f8aa494592b7912f6d30fbc2a839c892cb639
Esko Lahti <esko.lahti@reaktor.com>
2020-03-24 09:18:47 +0200

    clj-kondo

--- project.clj
+++ project.clj
@@ -55,6 +55,7 @@
   :profiles
   {:dev
             {:dependencies [[binaryage/devtools "1.0.0"]
+                            [clj-kondo "2019.11.23"]
                             [day8.re-frame/re-frame-10x "0.5.1"]
                             [day8.re-frame/tracing "0.5.3"]]
              :source-paths ["dev"]}
