b2eb66fc79165a97e727b5bcaf9d585c0568c2da
Esko Lahti <esko.lahti@reaktor.com>
2020-03-30 17:19:28 +0300

    Päätason grid-solut omina komponentteinaan

--- src/cljs/hakukohderyhmapalvelu/views/hakukohderyhmapalvelu_panel.cljs
+++ src/cljs/hakukohderyhmapalvelu/views/hakukohderyhmapalvelu_panel.cljs
@@ -15,6 +15,40 @@
                   "/ 1fr 1fr")
    :grid-gap "15px"})
 
+(defn- haku-search []
+  (let [input-id     "haku-search-input"
+        style-prefix "haku-search"]
+    [grid/input-with-label-and-control
+     {:control-component [checkbox/checkbox-with-label {:checkbox-id  "haku-search-checkbox"
+                                                        :cypressid    "haku-search-checkbox"
+                                                        :label        "Näytä myös päättyneet"
+                                                        :style-prefix (str style-prefix "-control")}]
+      :cypressid         "haku-search"
+      :input-component   [input/input-text {:cypressid    "haku-search-input"
+                                            :input-id     input-id
+                                            :placeholder  "Haun nimi"
+                                            :style-prefix (str style-prefix "-input")}]
+      :input-id          input-id
+      :style-prefix      style-prefix
+      :label             "Haku"}]))
+
+(defn- hakukohderyhma-create []
+  (let [button-width "100px"
+        cypressid    "hakukohderyhma-create"
+        input-id     "hakukohderyhma-create-input"
+        style-prefix "hakukohderyhma-create"]
+    [grid/input-and-button-without-top-row
+     {:button-component [b/button
+                         {:label        "Tallenna"
+                          :style-prefix (str style-prefix "-button")
+                          :styles       {:width button-width}}]
+      :input-component  [input/input-text
+                         {:cypressid    (str cypressid "-input")
+                          :input-id     input-id
+                          :placeholder  "Ryhmän nimi"
+                          :style-prefix (str style-prefix "-input")}]
+      :style-prefix     style-prefix}]))
+
 (def ^:private add-new-hakukohderyhma-link-styles
   (merge layout/vertical-align-center-styles
          {:grid-area    "hakukohderyhma-select-control"
@@ -26,52 +60,27 @@
                                 :href      ""
                                 :label     "Luo uusi ryhmä"}]])
 
+(defn- hakukohderyhma-select []
+  (let [cypressid    "hakukohderyhma-select"
+        input-id     "hakukohderyhma-select-input"
+        style-prefix "hakukohderyhma-select"]
+    [grid/input-with-label-and-control
+     {:control-component [add-new-hakukohderyhma-link
+                          {:cypressid (str cypressid "-add-new-hakukohderyhma")}]
+      :cypressid         cypressid
+      :input-component   [input/input-dropdown
+                          {:cypressid        (str cypressid "-dropdown")
+                           :style-prefix     (str style-prefix "-input")
+                           :unselected-label "Hakukohderyhmä"}]
+      :input-id          input-id
+      :style-prefix      style-prefix
+      :label             "Hakukohderyhmät"}]))
+
 (defn hakukohderyhmapalvelu-panel []
   [p/panel
    {:cypressid "hakukohderyhmapalvelu-panel"}
    "Hakukohderyhmien hallinta"
    [:div (stylefy/use-style hakukohderyhmapalvelu-grid-styles)
-    (let [input-id     "haku-search-input"
-          style-prefix "haku-search"]
-      [grid/input-with-label-and-control
-       {:control-component [checkbox/checkbox-with-label {:checkbox-id  "haku-search-checkbox"
-                                                          :cypressid    "haku-search-checkbox"
-                                                          :label        "Näytä myös päättyneet"
-                                                          :style-prefix (str style-prefix "-control")}]
-        :cypressid         "haku-search"
-        :input-component   [input/input-text {:cypressid    "haku-search-input"
-                                              :input-id     input-id
-                                              :placeholder  "Haun nimi"
-                                              :style-prefix (str style-prefix "-input")}]
-        :input-id          input-id
-        :style-prefix      style-prefix
-        :label             "Haku"}])
-    (let [button-width "100px"
-          cypressid    "hakukohderyham-create"
-          input-id     "hakukohderyhma-create-input"
-          style-prefix "hakukohderyhma-create"]
-      [grid/input-and-button-without-top-row
-       {:button-component [b/button
-                           {:label        "Tallenna"
-                            :style-prefix (str style-prefix "-button")
-                            :styles       {:width button-width}}]
-        :input-component  [input/input-text
-                           {:cypressid    (str cypressid "-input")
-                            :input-id     input-id
-                            :placeholder  "Ryhmän nimi"
-                            :style-prefix (str style-prefix "-input")}]
-        :style-prefix     style-prefix}])
-    (let [cypressid    "hakukohderyhma-select"
-          input-id     "hakukohderyhma-select-input"
-          style-prefix "hakukohderyhma-select"]
-      [grid/input-with-label-and-control
-       {:control-component [add-new-hakukohderyhma-link
-                            {:cypressid (str cypressid "-add-new-hakukohderyhma")}]
-        :cypressid         cypressid
-        :input-component   [input/input-dropdown
-                            {:cypressid        (str cypressid "-dropdown")
-                             :style-prefix     (str style-prefix "-input")
-                             :unselected-label "Hakukohderyhmä"}]
-        :input-id          input-id
-        :style-prefix      style-prefix
-        :label             "Hakukohderyhmät"}])]])
+    [haku-search]
+    [hakukohderyhma-create]
+    [hakukohderyhma-select]]])
