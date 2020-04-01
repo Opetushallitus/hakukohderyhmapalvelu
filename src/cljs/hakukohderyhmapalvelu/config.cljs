a8920bfeb833e825c08fddb84ee5ab361fe19f31
Esko Lahti <esko.lahti@reaktor.com>
2020-04-01 13:33:39 +0300

    Luetaan konfiguraatio globaalista js-muuttujasta

--- src/cljs/hakukohderyhmapalvelu/config.cljs
+++ src/cljs/hakukohderyhmapalvelu/config.cljs
@@ -1,4 +1,27 @@
-(ns hakukohderyhmapalvelu.config)
+(ns hakukohderyhmapalvelu.config
+  (:require [clojure.walk :as walk]
+            [hakukohderyhmapalvelu.public-config-schemas :as cs]
+            [schema.core :as s]))
 
-(def debug?
-  ^boolean goog.DEBUG)
+(def ^:private keywordize-vals #{:environment})
+
+(s/defn make-config :- cs/PublicConfig
+  []
+  (let [parsed-object (-> js/config
+                          (js->clj :keywordize-keys true))]
+    (walk/prewalk
+      (fn prewalk-config [x]
+        (cond->> x
+                 (map? x)
+                 (reduce-kv (fn reduce-val->kwd [acc k v]
+                              (assoc acc
+                                     k
+                                     (cond-> v
+                                             (contains? keywordize-vals k)
+                                             keyword)))
+                            {})))
+      parsed-object)))
+
+
+(def config
+  (make-config))
