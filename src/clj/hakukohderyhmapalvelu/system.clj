94df8bcb7d0b67a1ed4f3bb44db843dffff59768
Esko Lahti <esko.lahti@reaktor.com>
2020-04-06 16:02:03 +0300

    Konfiguraatio oli turhaan komponentti

--- src/clj/hakukohderyhmapalvelu/system.clj
+++ src/clj/hakukohderyhmapalvelu/system.clj
@@ -1,7 +1,7 @@
 (ns hakukohderyhmapalvelu.system
   (:require [com.stuartsierra.component :as component]
             [hakukohderyhmapalvelu.cas-client :as cas-client]
-            [hakukohderyhmapalvelu.config :as config]
+            [hakukohderyhmapalvelu.config :as c]
             [hakukohderyhmapalvelu.db :as db]
             [hakukohderyhmapalvelu.migrations :as migrations]
             [hakukohderyhmapalvelu.organisaatio-service :as organisaatio-service]
@@ -9,33 +9,25 @@
             [hakukohderyhmapalvelu.audit-log :as audit-log]))
 
 (defn hakukohderyhmapalvelu-system []
-  (component/system-map
-    :organisaatio-service-cas-client (component/using
-                                       (cas-client/map->CasClient {:service :organisaatio-service})
-                                       [:config])
+  (let [config (c/make-config)]
+    (component/system-map
+      :organisaatio-service-cas-client (cas-client/map->CasClient {:service :organisaatio-service
+                                                                   :config  config})
 
-    :config (config/map->Config {})
+      :audit-logger (audit-log/map->OpintopolkuAuditLogger {:config config})
 
-    :audit-logger (component/using
-                    (audit-log/map->OpintopolkuAuditLogger {})
-                    [:config])
+      :db (db/map->DbPool {:config config})
 
-    :db (component/using
-          (db/map->DbPool {})
-          [:config])
+      :migrations (component/using
+                    (migrations/map->Migrations {})
+                    [:db])
 
-    :migrations (component/using
-                  (migrations/map->Migrations {})
-                  [:db])
+      :organisaatio-service (component/using
+                              (organisaatio-service/map->OrganisaatioService {:config config})
+                              [:organisaatio-service-cas-client])
 
-    :organisaatio-service (component/using
-                            (organisaatio-service/map->OrganisaatioService {})
-                            [:organisaatio-service-cas-client
-                             :config])
-
-    :http-server (component/using
-                   (http/map->HttpServer {})
-                   [:config
-                    :db
-                    :migrations
-                    :organisaatio-service])))
+      :http-server (component/using
+                     (http/map->HttpServer {:config config})
+                     [:db
+                      :migrations
+                      :organisaatio-service]))))
