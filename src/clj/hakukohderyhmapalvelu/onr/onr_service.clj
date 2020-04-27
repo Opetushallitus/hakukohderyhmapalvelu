ba74d45401363c6694269e8aaa3b1c4fa001a780
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-04-27 09:26:31 +0300

    Lisää integraatio Oppijanumerorekisteriin
    Tätä tarvitaan sisäänkirjautumisessa ja audit-logituksessa tarvittavien
    tietojen saamiseksi.

---
+++ src/clj/hakukohderyhmapalvelu/onr/onr_service.clj
@@ -0,0 +1,44 @@
+(ns hakukohderyhmapalvelu.onr.onr-service
+  (:require [hakukohderyhmapalvelu.onr.onr-protocol :as onr-protocol]
+            [hakukohderyhmapalvelu.cas.cas-protocol :as cas-protocol]
+            [hakukohderyhmapalvelu.oph-url-properties :as url]
+            [schema.core :as s]))
+
+(defrecord HttpPersonService [onr-cas-client config]
+
+  onr-protocol/PersonService
+
+  (get-person [_ oid]
+    (let [url    (url/resolve-url :oppijanumerorekisteri.get-person config oid)]
+      (cas-protocol/get onr-cas-client url s/Any))))
+
+(def fake-onr-person {:oidHenkilo   "1.2.3.4.5.6"
+                      :hetu         "020202A0202"
+                      :etunimet     "Testi"
+                      :kutsumanimi  "Testi"
+                      :sukunimi     "Ihminen"
+                      :syntymaaika  "1941-06-16"
+                      :sukupuoli    "2"
+                      :kansalaisuus [{:kansalaisuusKoodi "246"}]
+                      :aidinkieli   {:id          "742310"
+                                     :kieliKoodi  "fi"
+                                     :kieliTyyppi "suomi"}
+                      :turvakielto  false
+                      :yksiloity    false
+                      :yksiloityVTJ false})
+
+(defrecord FakePersonService []
+  onr-protocol/PersonService
+
+  (get-person [this oid]
+    (condp = oid
+      "2.2.2" (merge fake-onr-person
+                     {:oidHenkilo "2.2.2"
+                      :turvakielto true
+                      :yksiloity   true
+                      :etunimet    "Ari"
+                      :kutsumanimi "Ari"
+                      :sukunimi    "Vatanen"
+                      :hetu         "141196-933S"})
+      (merge fake-onr-person
+             {:oidHenkilo oid}))))
