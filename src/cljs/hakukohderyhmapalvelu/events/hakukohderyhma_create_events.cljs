43fdc8b745ea4da9a482af805210159cd73047ee
Esko Lahti <esko.lahti@reaktor.com>
2020-10-14 13:13:19 +0300

    VTKU-204: fn-traced

--- src/cljs/hakukohderyhmapalvelu/events/hakukohderyhma_create_events.cljs
+++ src/cljs/hakukohderyhmapalvelu/events/hakukohderyhma_create_events.cljs
@@ -1,21 +1,22 @@
 (ns hakukohderyhmapalvelu.events.hakukohderyhma-create-events
   (:require [hakukohderyhmapalvelu.macros.event-macros :as events]
-            [hakukohderyhmapalvelu.api-schemas :as schemas]))
+            [hakukohderyhmapalvelu.api-schemas :as schemas]
+            [day8.re-frame.tracing :refer-macros [fn-traced]]))
 
 (events/reg-event-db-validating
   :hakukohderyhma-create/toggle-grid-visibility
-  (fn [db]
+  (fn-traced [db]
     (update-in db [:create-hakukohderyhma :visible?] not)))
 
 (events/reg-event-db-validating
   :hakukohderyhma-create/handle-save-hakukohderyhma
-  (fn [db [hakukohderyhma response]]
+  (fn-traced [db [hakukohderyhma response]]
     (println (str "DEBUG :hakukohderyhma-create/handle-save-hakukohderyhma: " {:hakukohderyhma hakukohderyhma :response response}))
     db))
 
 (events/reg-event-fx-validating
   :hakukohderyhma-create/save-hakukohderyhma
-  (fn [{db :db} [hakukohderyhma-name]]
+  (fn-traced [{db :db} [hakukohderyhma-name]]
     (let [http-request-id :hakukohderyhma-create/save-hakukohderyhma
           body            {:nimi {:fi hakukohderyhma-name}}]
       {:db   (update db :requests (fnil conj #{}) http-request-id)
