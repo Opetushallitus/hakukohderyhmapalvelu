de1ade73983a71f5f03b42942ac3dbf2afcb4005
Topi Penttinen <topi.penttinen@reaktor.com>
2021-03-02 13:45:40 +0200

    Change "Luo uusi ryhmä" anchor to button
    Change the underlying element of "Luo uusi ryhmä" from anchor into button.

--- cypress/selectors/hakukohderyhmanLisaysSelectors.ts
+++ cypress/selectors/hakukohderyhmanLisaysSelectors.ts
@@ -5,7 +5,7 @@ export const hakukohderyhmanLisaysClosedDropdownSelector =
   'span[cypressid=hakukohderyhma-select-dropdown-unselected-label]:visible'
 
 export const hakukohderyhmanLisaysLisaaUusiRyhmaLinkSelector =
-  'a[cypressid=hakukohderyhma-select-add-new-hakukohderyhma]:visible'
+  'button[cypressid=hakukohderyhma-select-add-new-hakukohderyhma]:visible'
 
 export const hakukohderyhmanLisaysNewHakukohderyhmaNameTextInputSelector =
   'input[cypressid=hakukohderyhma-create-input]:visible'
