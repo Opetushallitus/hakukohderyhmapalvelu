6fc4886eb8c58a7964ec2d6a6c4d90dee95bdd22
Topi Penttinen <topi.penttinen@reaktor.com>
2021-03-10 15:41:55 +0200

    HLE-1331 Create common utility functions for i18n operations

---
+++ src/cljs/hakukohderyhmapalvelu/i18n/utils.cljs
@@ -0,0 +1,34 @@
+(ns hakukohderyhmapalvelu.i18n.utils)
+
+;; Oletusjärjestys, jolla haetaan kielistettyä arvoa eri kielillä
+(def ^:private fi-order [:fi :sv :en])
+(def ^:private sv-order [:sv :fi :en])
+(def ^:private en-order [:en :fi :sv])
+
+(defn- order-for-lang [lang]
+  (case lang
+    :fi fi-order
+    :sv sv-order
+    :en en-order
+    :default fi-order))
+
+(defn get-with-fallback [m lang]
+  (->> (order-for-lang lang)
+       (map #(get m %))
+       (remove nil?)
+       first))
+
+(defn- item->option [lang label-field value-field item]
+  (let [localized (get item label-field)
+        value (get item value-field)
+        is-selected (get item :is-selected)]
+    {:label       (get-with-fallback localized lang)
+     :value       value
+     :is-selected is-selected}))
+
+(defn create-item->option-transformer [lang label-field value-field]
+  (partial
+    item->option
+    lang
+    label-field
+    value-field))
