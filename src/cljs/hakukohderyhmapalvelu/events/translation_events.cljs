d0095f5f0ec75ef5b90161ddb5e030694c69f3e6
Evan Miller <evan.miller@reaktor.fi>
2021-05-19 13:33:58 +0300

    Add translation base-url to properties

--- src/cljs/hakukohderyhmapalvelu/events/translation_events.cljs
+++ src/cljs/hakukohderyhmapalvelu/events/translation_events.cljs
@@ -1,7 +1,8 @@
 (ns hakukohderyhmapalvelu.events.translation-events
   (:require [clojure.string :as str]
             [day8.re-frame.tracing :refer-macros [fn-traced]]
-            [hakukohderyhmapalvelu.macros.event-macros :as events]))
+            [hakukohderyhmapalvelu.macros.event-macros :as events]
+            [hakukohderyhmapalvelu.urls :as urls]))
 
 (def get-remote-translations :translations/get-remote-translations)
 (def handle-get-remote-translations :translations/handle-get-remote-translations)
@@ -27,10 +28,13 @@
 (events/reg-event-fx-validating
   get-remote-translations
   (fn-traced [_ [locale]]
-             {:http {:method           :get
-                     :http-request-id  (keyword (str get-remote-translations "-" (name locale)))
-                     :path             (str "/lokalisointi/cxf/rest/v1/localisation")
-                     :search-params    [[:category "hakukohderyhmapalvelu"]
-                                        [:locale (name locale)]]
-                     ;TODO response schema?
-                     :response-handler [handle-get-remote-translations locale]}}))
+             (let [request-id (keyword (str get-remote-translations "-" (name locale)))
+                   url (str (urls/get-url :lokalisointi-service.baseurl)
+                            "/lokalisointi/cxf/rest/v1/localisation")]
+               {:http {:method           :get
+                       :http-request-id  request-id
+                       :path             url
+                       :search-params    [[:category "hakukohderyhmapalvelu"]
+                                          [:locale (name locale)]]
+                       ;TODO response schema?
+                       :response-handler [handle-get-remote-translations locale]}})))
