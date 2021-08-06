2daa77864402b71b51a1334c500e8919505ec9a6
Tatu Honkanen <tatu.honkanen@cybercom.com>
2021-08-06 10:56:59 +0300

    OY-2976 : Fix lint problem

--- cypress/selectors/hakukohderyhmanAsetustenSelectors.ts
+++ cypress/selectors/hakukohderyhmanAsetustenSelectors.ts
@@ -1,5 +1,6 @@
 export const rajaavaSelector = 'div[cypressid=rajaava-checkbox]:visible'
 
-export const yoAmmAutomHakukelpoisuusSelector = 'div[cypressid=yo-amm-autom-hakukelpoisuus-checkbox]:visible'
+export const yoAmmAutomHakukelpoisuusSelector =
+  'div[cypressid=yo-amm-autom-hakukelpoisuus-checkbox]:visible'
 
 export const maxHakukohteetSelector = 'input[cypressid=max-hakukohteet]:visible'
