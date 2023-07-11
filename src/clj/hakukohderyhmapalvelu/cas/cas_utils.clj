b1dc5d26522be03ffc7c445ba7ed855e1cbbd7c8
Mikko Siukola <mikko.siukola@cybercom.com>
2023-07-11 15:37:57 +0300

    OY-4231 Get cas-url from config

--- src/clj/hakukohderyhmapalvelu/cas/cas_utils.clj
+++ src/clj/hakukohderyhmapalvelu/cas/cas_utils.clj
@@ -8,10 +8,12 @@
 (defn create-cas-client [config service-url session-cookie-name]
   (let [{username :username
          password :password} config
-        cas-url (-> config :cas)
-        caller-id (-> config :oph-organisaatio-oid caller-id/make-caller-id)
-        cas-config (doto (new CasConfig$CasConfigBuilder username password cas-url service-url csrf-token caller-id "")
-                     (.setJsessionName session-cookie-name)
-                     (.build))
+        cas-url (-> config :cas :url)
+        caller-id (-> config
+                       :oph-organisaatio-oid
+                       (caller-id/make-caller-id))
+        cas-config (-> (new CasConfig$CasConfigBuilder username password cas-url service-url csrf-token caller-id "")
+                       (.setJsessionName session-cookie-name)
+                       (.build))
         cas-client (CasClientBuilder/build cas-config)]
     cas-client))
